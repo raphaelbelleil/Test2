eb8abb93fc8c42c8e91e92cc7e207b97971390e0
raphaelbelleil <raphael.belleil@2019.icam.fr>
2023-01-21 14:41:57 +0100

    2eme commit branche main

--- test.py
+++ test.py
@@ -2,8 +2,15 @@
 
 import random
 
-random.seed(0)
+# random.seed(0)
 
 liste = [random.randint(0,10) for i in range(20)]
 
 print (liste)
+
+
+# 2eme commit branche main
+
+liste2 = [random.randint(0,10) for i in range(20)]
+
+print (liste2)
